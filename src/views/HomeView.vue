31f9603ab8e3386688f74cd5a4f64d4c321359c3
Yamil Escobar <joseyamilej@hotmail.com>
2022-11-13 22:10:19 +0100

    Composable - usePlacesStore

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,9 +6,20 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { usePlacesStore } from '@/composables';
 
 export default defineComponent({
   name: 'HomeView',
   components: { },
+  setup() {
+    
+    usePlacesStore();
+
+
+  } 
+
+
+
+
 });
 </script>
