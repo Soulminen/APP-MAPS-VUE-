fe4bd2e4b9c34ae836ac79815491f1ad61536226
Yamil Escobar <joseyamilej@hotmail.com>
2022-11-15 23:32:06 +0100

    loading and reference to the map container

---
+++ src/components/mapview/MapView.ts
@@ -0,0 +1,24 @@
+import { defineComponent, ref, onMounted } from 'vue';
+import { usePlacesStore } from '../../composables/usePlacesStore';
+
+export default defineComponent({
+    name: 'MapView',
+    setup() {
+        
+
+        const mapElement = ref<HTMLDivElement>();
+        const { userLocation, isUserlocationReady } = usePlacesStore();
+
+        onMounted(() => {
+            console.log( mapElement.value );
+            
+        })
+
+        return {
+
+            isUserlocationReady,
+            mapElement,
+
+        }
+    }
+});
