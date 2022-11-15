513afb9c52d2e3bbe95ffbeef2970e471272373d
Yamil Escobar <joseyamilej@hotmail.com>
2022-11-15 23:58:04 +0100

    Show map Mapbox

--- src/components/mapview/MapView.ts
+++ src/components/mapview/MapView.ts
@@ -1,5 +1,8 @@
 import { defineComponent, ref, onMounted } from 'vue';
 import { usePlacesStore } from '../../composables/usePlacesStore';
+import mapboxgl from 'mapbox-gl';
+
+
 
 export default defineComponent({
     name: 'MapView',
@@ -9,10 +12,27 @@ export default defineComponent({
         const mapElement = ref<HTMLDivElement>();
         const { userLocation, isUserlocationReady } = usePlacesStore();
 
+        const initMap = () => {
+            if ( !mapElement.value ) throw new Error('Div Element no exits');
+            if ( !userLocation.value ) throw new Error('user location no existe');
+
+            const map = new mapboxgl.Map({
+                container: mapElement.value, // container ID
+                style: 'mapbox://styles/mapbox/streets-v11', // style URL
+                center: userLocation.value, // starting position [lng, lat]
+                zoom: 15, // starting zoom
+                });
+
+        }
+
         onMounted(() => {
-            console.log( mapElement.value );
+           if ( isUserlocationReady ) 
+           return initMap();
+           
+           console.log('No tengo localizacion aun');
+           
             
-        })
+        });
 
         return {
 
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import mapboxgl from 'mapbox-gl';
 
-
+mapboxgl.accessToken = 'pk.eyJ1Ijoic291bG1pbmVuIiwiYSI6ImNsYWlzbjV6ajA1dmMzb3FxY3l0eHgwOWQifQ.QarrytoLORzaYmjpM7UhZQ';
 
 if ( !navigator.geolocation) {
     alert("Tu navegador no soporta el GeoLocation");
