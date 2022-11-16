5d1faa9dc37b076e34cd00ceb7cf9fab13dbf9f5
Yamil Escobar <joseyamilej@hotmail.com>
2022-11-16 22:20:52 +0100

    SearchResults - component design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
 <template>
   
     <MapView />
-
+    <SearchBar />
     <MyLocationBtn />
 
 </template>
@@ -12,13 +12,14 @@
 import { defineComponent } from 'vue';
 import { usePlacesStore } from '@/composables';
 
-import  MapView  from "@/components/mapview/MapView.vue";
+import MapView  from "@/components/mapview/MapView.vue";
+import SearchBar  from "@/components/searchbar/SearchBar.vue";
 import MyLocationBtn from '@/components/my-location.btn/MyLocationBtn.vue';
 
 
 export default defineComponent({
   name: 'HomeView',
-  components: { MapView, MyLocationBtn },
+  components: { MapView, MyLocationBtn, SearchBar },
   setup() {
     
     usePlacesStore();
