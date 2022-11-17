2b477ebad99146a3251bc0efbe6930b713497b19
Yamil Escobar <joseyamilej@hotmail.com>
2022-11-17 23:02:32 +0100

    Implementing a debounce manually

---
+++ src/composables/useMapStore.ts
@@ -0,0 +1,31 @@
+import { useStore } from 'vuex';
+import { computed } from 'vue';
+import { StateInterface } from '@/store';
+import mapboxgl from 'mapbox-gl';
+
+
+
+export const useMapStore = () => {
+
+    const store = useStore<StateInterface>();
+
+
+    return {
+
+        map: computed( () => store.state.map.map ),
+        distance: computed( () => store.state.map.distance ),
+        duration: computed( () => store.state.map.duration ),
+
+        // Getters
+        isMapReady: computed<boolean>( () => store.getters['map/isMapReady'] ),
+
+        // Mutations
+        setMap: ( map: mapboxgl.Map ) => store.commit('map/setMap', map ), 
+
+        // Actions
+
+
+    }
+
+
+}
