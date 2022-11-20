48cb537ec0986391e0c805bfb1aac25c58106c6d
Yamil Escobar <joseyamilej@hotmail.com>
2022-11-20 13:23:13 +0100

    Show route in the map

--- src/store/map/actions.ts
+++ src/store/map/actions.ts
@@ -12,7 +12,9 @@ const actions: ActionTree<MapState, StateInterface> = {
         
         const resp = await directionsApi.get<DirectionsResponse>(`${ start.join(',')};${ end.join(',')}`);
 
-        console.log( resp.data.routes[0].geometry.coordinates );
+        //TODO: state distance
+
+        commit('setRoutePolyline', resp.data.routes[0].geometry.coordinates);
         
     }
 }
